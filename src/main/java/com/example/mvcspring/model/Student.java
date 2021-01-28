2547b39e5fbb5e73773ca56c0c951ccdfb549964
[badrri-narayanan] <[badrri1995@gmail.com]>
2021-01-28 21:49:43 +0530

    overrided the hashcode and equals method for student entity

--- src/main/java/com/example/mvcspring/model/Student.java
+++ src/main/java/com/example/mvcspring/model/Student.java
@@ -12,8 +12,8 @@ import javax.persistence.Table;
 public class Student {
 
 	@Id
-	@Column(name="id", updatable = false, nullable = false)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(name="first_name")
@@ -63,13 +63,47 @@ public class Student {
 		this.email = email;
 	}
 
-	public Student() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Student))
+			return false;
+		Student other = (Student) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
 	}
 	
 }
